456da15c85825bbf6e6239155d2fc156d26eb156
faceslog <66552663+faceslog@users.noreply.github.com>
2023-02-23 02:49:25 -0800

    Create Cloudflare Worker

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import { Router } from 'itty-router'
+import { createEmbed } from './utils';
+
+
+// Create a new router
+const router = Router()
+
+/*
+Our index route, a simple hello world.
+*/
+router.get("/", () => {
+  return new Response("Welcome to the Duplicati Proxy Notifier powered by a Cloudflared worker !")
+})
+
+router.post("/api/:name/:channel/:webhook", async (request) => {
+
+  const params = request.params;
+
+  const name = decodeURIComponent(params.name);
+  const channelId = decodeURIComponent(params.channel);
+  const webhookId = decodeURIComponent(params.webhook);
+  const webhookUrl = `https://discord.com/api/webhooks/${channelId}/${webhookId}`
+
+  // If the POST data is JSON then attach it to our response.
+  if (request.headers.get("Content-Type") != "application/json")    
+  { 
+    return new Response(JSON.stringify({ status: 404, message: "No content provided" }), {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      }
+    });
+  }
+
+  let body = await request.json()
+  let discordPayload = createEmbed(name, body.Data);
+
+  const res = await fetch(webhookUrl, { 
+    method: 'POST', 
+    headers: { 'Content-Type': 'application/json' }, 
+    body: JSON.stringify(discordPayload)
+  });
+
+  if(res.status != 200 && res.status != 204) 
+  {
+    return new Response(JSON.stringify({ status: res.status, message: res.statusText }), {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      }
+    })
+  } 
+
+  return new Response(JSON.stringify({ status: 200, message: 'Discord webhook triggered' }), {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    }
+  })
+
+});
+
+/*
+This is the last route we define, it will match anything that hasn't hit a route we've defined
+above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
+*/
+router.all("*", () => new Response("404, not found!", { status: 404 }))
+
+/*
+This snippet ties our worker to the router we deifned above, all incoming requests
+are passed to the router where your routes are called and the response is sent.
+*/
+addEventListener('fetch', (e) => {
+  e.respondWith(router.handle(e.request))
+})
---
+++ src/utils.js
@@ -0,0 +1,133 @@
+function convertSize(num) {
+
+    num = parseInt(num);
+    const units = ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"];
+  
+    for (let i = 0; i < units.length; i++) {
+      
+      if (Math.abs(num) < 1024.0) {
+        return `${num.toFixed(1)}${units[i]}B`;
+      }
+      num /= 1024.0;
+    }
+  
+    return `${num.toFixed(1)}YiB`;
+}
+
+function convertTime(timeStr) {
+
+    timeStr = timeStr.split(':');
+
+    let duration = '';
+
+    if (timeStr[0] != '00') {
+        duration += `${duration} ${timeStr[0]} Hrs `
+    }
+        
+    if(timeStr[1] != '00') {
+        duration += `${duration} ${timeStr[1]} Mins `
+    }
+
+    if(timeStr[2] != '00') {
+        seconds = Math.round(timeStr[2]);
+        duration = `${duration} ${seconds} Secs`;
+    }
+
+    return duration;
+}
+
+function getColor(parsedResultStr) {
+    
+    let color = {}
+    color['Success'] = 0x7CFC00;
+    color['Unknown'] = 0x909090;
+    color['Warning'] = 0xFFBF00;
+    color['Error'] = 0xFF0000;
+    color['FATAL'] = 0xFF0000;
+
+    return color[parsedResultStr];
+}
+
+function getIcon(parsedResultStr) {
+    
+    let icon = {}
+    icon['Success'] = ':white_check_mark:';
+    icon['Warning'] = ':warning:';
+    icon['Error'] = ':no_entry:';
+    icon['Uknown'] = ':grey_question:';
+    icon['FATAL'] = ':fire:';
+
+    console.log(parsedResultStr);
+    console.log(icon[parsedResultStr]);
+
+    return icon[parsedResultStr];
+}
+
+export function createEmbed(name, data) {
+
+   const icon = getIcon(data.ParsedResult);
+
+    let embed = {
+        embeds: [
+            {
+                title: `${icon} ${data.MainOperation}: ${name} -> ${data.ParsedResult} ${icon}`,
+                description: "",
+                color: getColor(data.ParsedResult),
+                author: {
+                    name: "Duplicati Proxy Notifier",
+                    url: "https://discord.com"
+                },
+                footer: {
+                    text: "Duplicati - Cloudflare Proxy Notifier - faceslog.com",
+                    icon_url: "https://faceslog.com/landing/profile.webp"
+                },
+                fields: [
+                    {
+                        name: "Started",
+                        value: new Date(data.BeginTime).toUTCString(),
+                        inline: false
+                    },
+                    {
+                        name: "Time Taken",
+                        value: convertTime(data.Duration),
+                        inline: false
+                    },
+                    {
+                        name: "No. Files",
+                        value: data.ExaminedFiles,
+                        inline: true
+                    },
+                    {
+                        name: "Added Files",
+                        value: data.AddedFiles,
+                        inline: true
+                    },
+                    {
+                        name: "Modified Files",
+                        value: data.ModifiedFiles,
+                        inline: true
+                    },
+                    {
+                        name: "Total Size",
+                        value: convertSize(data.SizeOfExaminedFiles),
+                        inline: true
+                    },
+                    {
+                        name: "Added Size",
+                        value: convertSize(data.SizeOfAddedFiles),
+                        inline: true
+                    },
+                    {
+                        name: "Modified Size",
+                        value: convertSize(data.SizeOfModifiedFiles),
+                        inline: true
+                    }
+                ]
+            }
+        ]
+    }
+
+    return embed;
+}
+
+
